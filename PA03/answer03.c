3c12ffa7623f26ba525a211d12ffdeec566d4a0a
Shubham Sandeep Rastogi <rastogis@ecegrid-thin6.ecn.purdue.edu>
2014-09-15 15:41:15 -0400

    PA03 first push

---
+++ PA03/answer03.c
@@ -0,0 +1,51 @@
+#include"answer03.h"
+#include<string.h>
+
+char * strcat_ex(char * * dest, int * n, const char * src)
+{
+	int dest_len = 0;
+	int src_len = 0;
+	char * new_dest;
+	src_len = strlen(src);
+	if(*dest != NULL)
+	{
+		dest_len = strlen(*dest);
+	}
+	if(*dest == NULL || dest_len + src_len + 1 > *n)
+	{
+		new_dest = (char *)malloc(sizeof(char *) * (1 + 2 * (dest_len + src_len)));
+		//*n = strlen(new_dest);
+		strcpy(new_dest, *dest);
+		//free(*dest);
+		*dest = new_dest;
+		strcat(*dest, src);
+		//free(*dest);
+	}
+	else
+	{
+		strcat(*dest, src);
+	}
+	return(*dest);
+}
+
+char * * explode(const char * str, const char * delims, int * arrLen)
+{
+	return(NULL);
+}
+char * implode(char * * strArr, int len, const char * glue)
+{
+	return(NULL);
+}
+void sortStringArray(char * * arrString, int len)
+{
+
+}
+void sortStringCharacters(char * str)
+{
+
+}
+void destroyStringArray(char * * strArr, int len)
+{
+
+}
+
---
+++ PA03/pa03.c
@@ -0,0 +1,13 @@
+#include"answer03.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc,char** argv)
+{
+	char * dest = "Shubham";
+	int n = strlen(dest);
+	char * src = " Rastogi";
+	printf("The Concatenated String is %s\n\n",strcat_ex(&dest, &n, src));
+	return(EXIT_SUCCESS);
+}
