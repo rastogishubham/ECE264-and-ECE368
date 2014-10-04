07468d60fdd389eae51643cadfb7cd5d2052c81f
Shubham Sandeep Rastogi <rastogis@ecegrid-thin1.ecn.purdue.edu>
2014-10-03 20:01:36 -0400

    homewrok done

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -4,7 +4,7 @@
 int main(int argc, char * * argv)
 {
 	int ind = 0;
-	for(ind = 0; ind < argc; ind++)
+	for(ind = 1; ind < argc; ind++)
 	{
 		if(strcmp(argv[ind], "--help") == 0)
 		{
@@ -18,10 +18,15 @@ int main(int argc, char * * argv)
 	}
 	FILE * fp = stdin;
 	ind = 0;
-	char str[2000];
-	for(ind = 0; ind < argc - 1; ind++)
+	int flag_invert = 0;
+	int flag_line = 0;
+	int flag_quiet = 0;
+	int line_num = 1;
+	char str[2001];
+	int return_val = 0;
+	for(ind = 1; ind < argc - 1; ind++)
 	{
-		if(strcmp(argv[ind], "-v") != 0 || strcmp(argv[ind], "-n") != 0 || strcmp(argv[ind], "-q") != 0 || strcmp(argv[ind], "--invert-match") != 0 || strcmp(argv[ind], "--line-number") != 0 || strcmp(argv[ind], "--quiet") != 0)
+		if(strcmp(argv[ind], "-v") != 0 && strcmp(argv[ind], "-n") != 0 && strcmp(argv[ind], "-q") != 0 && strcmp(argv[ind], "--invert-match") != 0 && strcmp(argv[ind], "--line-number") != 0 && strcmp(argv[ind], "--quiet") != 0)
 		{
 			fprintf(stderr, "grep error %s is bogus command-line argument", argv[ind]);
 			return 2;
@@ -32,12 +37,67 @@ int main(int argc, char * * argv)
 		fprintf(stderr, "grep error PATTERN begins with '-'\n");
 		return 2;
 	}
-	while(!feof(fp))
+	for(ind = 0; ind < argc; ind++)
 	{
-		fgets(str, 2000, fp);
-		if(strstr(str, argv[argc - 1]) != NULL)
+		if(strcmp(argv[ind], "-v") == 0 || strcmp(argv[ind], "--invert-match") == 0)
 		{
+			flag_invert = 1;
+		}
+		if(strcmp(argv[ind], "-n") == 0 || strcmp(argv[ind], "--line-number") == 0)
+		{
+		//	printf("Line number found");
+			flag_line = 1;
+		}
+		if(strcmp(argv[ind], "-q") == 0 || strcmp(argv[ind], "--quiet") == 0)
+		{
+//			printf ("Quiet found!\n");			
+
+			flag_quiet = 1;
 		}
 	}
-	return EXIT_SUCCESS;
+	while(fgets(str, 2001, fp) != NULL)
+	{
+		//printf("fgets not null \n");
+		if(strstr(str, argv[argc - 1]) != NULL && flag_quiet == 1)
+		{
+	//		printf ("Found match and quiet!\n");
+			return_val =  0;
+		}
+		if(strstr(str, argv[argc - 1]) == NULL && flag_quiet == 1)
+		{
+
+			//printf ("did not Find match and quiet!\n");
+			return_val = 0;
+		}
+		if(flag_invert == 1 && strstr(str, argv[argc - 1]) == NULL && flag_line == 1 && flag_quiet == 0)
+		{
+			//printf("a");
+			printf("%d: ", line_num);
+			printf("%s", str);
+			return_val = 0;
+		}
+		if(flag_invert == 1 && strstr(str, argv[argc - 1]) == NULL && flag_line == 0 && flag_quiet == 0)
+		{	
+			//printf("b");
+			printf("%s", str);
+			return_val = 0;
+		}
+		if(flag_invert == 0 && flag_line == 0 && flag_quiet == 0  && strstr(str, argv[argc - 1]) != NULL)
+		{	
+			//printf("c");
+			printf("%s", str);
+			return_val = 0;
+		}
+		if(flag_invert == 0 && flag_line == 1 && flag_quiet == 0 && strstr(str, argv[argc - 1]) != NULL)
+		{
+			//printf("d");
+			printf("%d: ", line_num);
+			printf("%s", str);
+			return_val = 0;
+		}
+		line_num++;
+		//fclose(fp);
+	}
+	fclose(fp);
+	return return_val;
 }
