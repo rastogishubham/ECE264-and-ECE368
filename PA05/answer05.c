8c5fe9ef9258be6a9528ed99372fd6a3e395e192
Shubham Sandeep Rastogi <rastogis@ecegrid-thin1.ecn.purdue.edu>
2014-10-08 01:51:08 -0400

    all functions working fine

--- PA05/answer05.c
+++ PA05/answer05.c
@@ -1,6 +1,7 @@
 #include"answer05.h"
 #include<stdio.h>
 #include<stdlib.h>
+//function definitions
 void partitionAllHelper(int*, int, int);
 void printPartition(int*, int);
 void partitionIncreasingHelper(int *, int, int);
@@ -8,6 +9,39 @@ void partitionDecreasingHelper(int *, int, int);
 void partitionOddHelper(int * , int , int);
 void partitionEvenHelper(int *, int, int);
 void partitionOddAndEvenHelper(int*, int, int);
+void partititonPrimeHelper(int *, int, int);
+//code for function partitionPrimeHelper based on code from Prof. Lu's book.
+void partitionPrimeHelper(int * buffer, int ind, int left)
+{
+	//variable declerations
+	int val;
+	int lcv = 0;
+	int count = 0;
+	//base case
+	if(left == 0)
+	{
+		printPartition(buffer, ind);
+		return;
+	}
+	//recursive case
+	for(val = 1; val <= left; val ++)
+	{
+		count = 0;
+		for(lcv = 1; lcv <= val; lcv++)
+		{
+			if(val % lcv == 0)
+			{
+				count ++;
+			}
+		}
+		if(count == 2)
+		{
+			buffer[ind] = val;
+			partitionPrimeHelper(buffer, ind + 1, left - val);
+		}
+	}
+}
+//code for function partitionOddAndEvenHelper based on code from Prof. Lu's book
 void partitionOddAndEvenHelper(int * buffer, int ind, int left)
 {
 	int val;
@@ -34,6 +68,7 @@ void partitionOddAndEvenHelper(int * buffer, int ind, int left)
 		}
 	}
 }
+//code for function partitionEvenHelper based on code from Prof. Lu's book
 void partitionEvenHelper(int * buffer, int ind, int left)
 {
 	int val;
@@ -48,6 +83,7 @@ void partitionEvenHelper(int * buffer, int ind, int left)
 		partitionEvenHelper(buffer, ind + 1, left - val);
 	}
 }
+//code for function partitionOddHelper based on code from Prof. Lu's book
 void partitionOddHelper(int * buffer, int ind, int left)
 {
 	int val;
@@ -62,6 +98,7 @@ void partitionOddHelper(int * buffer, int ind, int left)
 		partitionOddHelper(buffer, ind + 1, left - val);
 	}
 }
+//code for function partitionDecreasingHelper based on code from Prof. Lu's book
 void partitionDecreasingHelper(int * buffer, int ind, int left)
 {
 	int val = 0;
@@ -81,6 +118,7 @@ void partitionDecreasingHelper(int * buffer, int ind, int left)
 		partitionDecreasingHelper(buffer, ind + 1,  left - val);
 	}
 }
+//coe for function partitionIncreasingHelper based on code from Prof. Lu's book
 void partitionIncreasingHelper(int *buffer, int ind, int left)
 {
 	int val;
@@ -100,7 +138,7 @@ void partitionIncreasingHelper(int *buffer, int ind, int left)
 		partitionIncreasingHelper(buffer, ind + 1, left - val);
 	}
 }
-
+//code for function printPartition based on code from Prof. Lu's book
 void printPartition(int *buffer, int length)
 {
 	int ind;
@@ -111,6 +149,7 @@ void printPartition(int *buffer, int length)
 	}
 	printf("%d\n", buffer[length - 1]);
 }
+//code for function partitionAllHelper based on code from Prof. Lu's book
 void partitionAllHelper(int *buffer, int ind, int left)
 {
 	int val;
@@ -125,6 +164,7 @@ void partitionAllHelper(int *buffer, int ind, int left)
 		partitionAllHelper(buffer, ind + 1, left - val);
 	}
 }
+//code for function partitionAll based on code from Prof. Lu's book
 void partitionAll(int value)
 {
 	int *buffer;
@@ -133,7 +173,7 @@ void partitionAll(int value)
 	free(buffer);
 	return;
 }
-
+//code for function partitionIncreasing based on code from Prof. Lu's book
 void partitionIncreasing(int value)
 {
 	int *buffer;
@@ -142,34 +182,43 @@ void partitionIncreasing(int value)
 	free(buffer);
 	return;
 }
-
+//code for function partitionDecreasing base on code from Prof. Lu's book
 void partitionDecreasing(int value)
 {
 	int *buffer = malloc(sizeof(int) * value);
 	partitionDecreasingHelper(buffer, 0, value);
+	free(buffer);
 	return;
 }
-
+//code for function partitionOdd based on code from Prof. Lu's book
 void partitionOdd(int value)
 {
 	int * buffer = malloc(sizeof(int) * value);
 	partitionOddHelper(buffer, 0, value);
+	free(buffer);
 	return;
 }
+//code for function partitionEven based on code from Prof. Lu's book
 void partitionEven(int value)
 {
 	int *buffer = malloc(sizeof(int) * value);
 	partitionEvenHelper(buffer, 0, value);
+	free(buffer);
 	return;
 }
-
+//code for function partitionOddAndEven based on code from Prof. Lu's book
 void partitionOddAndEven(int value)
 {	
 	int * buffer = malloc(sizeof(int) * value);
 	partitionOddAndEvenHelper(buffer, 0, value);
+	free(buffer);
 	return;
 }
+//code for function partitionPrime based on code from Prof. Lu's book
 void partitionPrime(int value)
 {
+	int * buffer = malloc(sizeof(int) * value);
+	partitionPrimeHelper(buffer, 0, value);
+	free(buffer);
 	return;
 }
--- PA05/pa05.c
+++ PA05/pa05.c
@@ -5,11 +5,18 @@
 
 int main(int argc, char ** argv)
 {
-	//partitionAll(5);
-	//partitionIncreasing(4);
-	//partitionDecreasing(5);
-	//partitionOdd(5);
-	//partitionEven(6);
+	partitionAll(5);
+	printf("\n");
+	partitionIncreasing(4);
+	printf("\n");
+	partitionDecreasing(5);
+	printf("\n");
+	partitionOdd(5);
+	printf("\n");
+	partitionEven(6);
+	printf("\n");
 	partitionOddAndEven(5);
+	printf("\n");
+	partitionPrime(5);
 	return EXIT_SUCCESS;
 }
