7c41a9cb0230dbd9008573361e21fa29ec96dff5
Shubham Sandeep Rastogi <rastogis@ecegrid-thin4.ecn.purdue.edu>
2015-02-01 18:58:36 -0500

    simulator tested and working - no memory leaks

--- PRJ1_A/proj1_A_s15.c
+++ PRJ1_A/proj1_A_s15.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
-//
+#include<time.h>
 typedef struct Queue_Node
 {
 	double arr_time;
@@ -12,9 +12,11 @@ typedef struct Queue_Node
 }Queue;
 
 //fuction declarations
-Queue * mode1(double lamda_0, double lambda_1, double mu, int total_tasks);
+void mode1(double lamda_0, double lambda_1, double mu, int total_tasks);
 Queue * Queue_Node_Create(int priority, double arr_time, double service_time);
 double calculate_inter_arrival_time(double lambda, double r);
+int queue_len(Queue * queue);
+void simulator(Queue * FEL1, Queue * FEL2, int total_tasks);
 
 Queue * Queue_Node_Create(int priority, double arr_time, double service_time)
 {
@@ -40,10 +42,13 @@ void Queue_destroy(Queue * queue)
 
 double calculate_r(double mu)
 {
-	double x = (double) rand() / RAND_MAX;
-	//printf("\n x = %f\n", x);
-	double r = (-1 / mu) * log(1 - x);
-	//printf("\n Random R = %f\n", r);
+	double x;
+	double r;
+	//srand(time(NULL));
+	x = (double) rand() / RAND_MAX;
+	printf("\n x = %f\n", x);
+	r = (-1 / mu) * log(1 - x);
+//	printf("\n Random R = %f\n", r);
 	return r;
 }
 
@@ -83,9 +88,9 @@ int main(int argc, char * * argv)
 		double lambda_1 = atof(argv[2]);
 		double mu = atof(argv[3]);
 		int total_tasks = atoi(argv[4]) * 2;
-		Queue * FEL1 = mode1(lambda_0, lambda_1, mu, total_tasks);
+		mode1(lambda_0, lambda_1, mu, total_tasks);
 //		list_print(FEL1);
-		Queue_destroy(FEL1);
+//		Queue_destroy(FEL1);
 	}
 	else
 	{
@@ -106,12 +111,12 @@ Queue * create_FEL(int priority, double arrival_time, double service_time, Queue
 	return FEL;
 }
 
-Queue * mode1(double lambda_0, double lambda_1, double mu, int total_tasks)
+void mode1(double lambda_0, double lambda_1, double mu, int total_tasks)
 {
 	if(lambda_0 + lambda_1 > mu)
 	{
 		printf("Enter values so that lambda_1 + lambda_0 is less than mu\n");
-		return NULL;
+		return;
 	}
 	int lcv = 0;
 	double inter_arr_time = 0;
@@ -121,26 +126,37 @@ Queue * mode1(double lambda_0, double lambda_1, double mu, int total_tasks)
 	double tot_time_1 = 0;
 	Queue * FEL1 = NULL;
 	Queue * FEL2 = NULL;
+	srand(time(NULL));
 	for(lcv = 0; lcv < total_tasks / 2; lcv++)
 	{
 		r = calculate_r(lambda_0);
+		printf("\nRandom R for lam 0 inter arr time%f\n", r);
 		inter_arr_time = calculate_inter_arrival_time(lambda_0, r);
 		r = calculate_r(mu);
+		printf("\nRandom R for service time for 0 %f\n", r);
 		service_time = calculate_service_time(mu, r);
 		tot_time_0 += inter_arr_time;
-		FEL1 = create_FEL(0, tot_time, service_time, FEL1);
+		printf("\nArrival time of 0 %f\n", tot_time_0);
+		printf("\nService time of 0 %f\n", service_time);
+		FEL1 = create_FEL(0, tot_time_0, service_time, FEL1);
 	}
 	for(lcv = 0; lcv < total_tasks / 2; lcv++)
 	{
 		r = calculate_r(lambda_1);
+                printf("\nRandom R for lam 1 inter arr time%f\n", r);
 		inter_arr_time = calculate_inter_arrival_time(lambda_1, r);
 		r = calculate_r(mu);
+                printf("\nRandom R for service time for 1 %f\n", r);
 		service_time = calculate_service_time(mu, r);
 		tot_time_1 += inter_arr_time;
-		FEL2 = create_FEL(1, tot_time, service_time, FEL2);
+		printf("\nArrival time of 1 %f\n", tot_time_1);
+		printf("\nService time of 1 %f\n", service_time);
+		FEL2 = create_FEL(1, tot_time_1, service_time, FEL2);
 	}
+	simulator(FEL1, FEL2, total_tasks);
+	Queue_destroy(FEL1);
 	Queue_destroy(FEL2);
-	return FEL1;
+	return;
 }
 
 void simulator(Queue * FEL1, Queue * FEL2, int total_tasks)
@@ -160,40 +176,41 @@ void simulator(Queue * FEL1, Queue * FEL2, int total_tasks)
 	int tot_wait_1 = 0;
 	double avg_wait_0 = 0;
 	double avg_wait_1 = 0;
+	int total_tasks_cpy = total_tasks;
 	while(total_tasks > 0)
 	{
 		queue_0_len = queue_len(temp_queue_0);
 		queue_1_len = queue_len(temp_queue_1);
 		tot_queue_len += queue_0_len + queue_1_len;
-		if(total_time == FEL1 -> arr_time)
+		if(FEL1 != NULL && total_time == FEL1 -> arr_time)
 		{
-			temp_queue_0 = create_list(0, FEL1 -> arr_time, FEL1 -> service_time, temp_queue_0);
-			if(flag == 1)
-			{
-				server_time = temp_queue_0 -> service_time;
-				tot_service_time += server_time;
-				flag = 0;
-				tot_wait_0 += (total_time - temp_queue_0 -> arr_time);
-				Queue * p = temp_queue_0 -> next;
-				free(temp_queue_0);
-				temp_queue_0 = p;
-			}
+			temp_queue_0 = create_FEL(0, FEL1 -> arr_time, FEL1 -> service_time, temp_queue_0);
 			FEL1 = FEL1 -> next;
 		}
-		if(total_time == FEL2 -> arr_time)
+		if(flag == 1 && temp_queue_0 != NULL)
 		{
-			temp_queue_1 = create_list(0, FEL2 -> arr_time, FEL2 -> service_time, temp_queue_1);
-			if(flag == 1 && temp_queue_0 == NULL)
-			{
-				server_time = temp_queue_1 -> service_time;
-				tot_service_time += server_time;
-				flag = 0;
-				tot_wait_1 += (total_time - temp_queue_0 -> arr_time);
-				Queue * p = temp_queue_1 -> next;
-				free(temp_queue_1);
-				temp_queue_1 = p;
-			}
+			server_time = temp_queue_0 -> service_time;
+			tot_service_time += server_time;
+			flag = 0;
+			tot_wait_0 += (total_time - temp_queue_0 -> arr_time);
+			Queue * p = temp_queue_0 -> next;
+			free(temp_queue_0);
+			temp_queue_0 = p;
+		}
+		if(FEL2 != NULL && total_time == FEL2 -> arr_time)
+		{
+			temp_queue_1 = create_FEL(0, FEL2 -> arr_time, FEL2 -> service_time, temp_queue_1);
 			FEL2 = FEL2 -> next;
+		}
+		if(flag == 1 && temp_queue_0 == NULL && temp_queue_1 != NULL)
+		{
+			server_time = temp_queue_1 -> service_time;
+			tot_service_time += server_time;
+			flag = 0;
+			tot_wait_1 += (total_time - temp_queue_1 -> arr_time);
+			Queue * p = temp_queue_1 -> next;
+			free(temp_queue_1);
+			temp_queue_1 = p;
 		}
 		total_time++;
 		if(flag == 0)
@@ -206,10 +223,18 @@ void simulator(Queue * FEL1, Queue * FEL2, int total_tasks)
 			}
 		}
 	}
-	avg_CPU_util = tot_service_time / total_time;
-	avg_queue_len = tot_queue_len / (total_time + 1);
-	avg_wait_0 = tot_wait_0 / (total_tasks / 2);
-	avg_wait_1 = tot_wait_1 / (total_tasks / 2);
+	avg_CPU_util = (double) tot_service_time / (double) total_time;
+	avg_queue_len = (double) tot_queue_len / (double) (total_time + 1);
+	avg_wait_0 = (double) tot_wait_0 / (double) (total_tasks_cpy / 2);
+	avg_wait_1 = (double) tot_wait_1 / (double) (total_tasks_cpy / 2);
+	printf("tot_wait_0 %d\n", tot_wait_0);
+	printf("avg wait for 0 %f\n", avg_wait_0);
+        printf("tot_wait_1 %d\n", tot_wait_1);
+        printf("avg wait for 1 %f\n", avg_wait_1);
+	printf("avg cpu util %f\n", avg_CPU_util);
+        printf("avg queue len %f\n", avg_queue_len);
+
+
 }
 
 int queue_len(Queue * queue)
