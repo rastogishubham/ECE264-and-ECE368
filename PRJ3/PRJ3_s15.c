dbf359835c97ecb0f3105b141994af17e0e0a30d
Shubham Sandeep Rastogi <rastogis@ecegrid-thin3.ecn.purdue.edu>
2015-04-23 10:09:15 -0400

    input done properly, dense and sparse graphs made

--- PRJ3/PRJ3_s15.c
+++ PRJ3/PRJ3_s15.c
@@ -5,25 +5,30 @@
 
 void destroy_arr(int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income);
 void print_arr(long int size, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income);
-void create_arr(long int size, char * file, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income);
+void create_arr(int size, char * file, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income, FILE * fp);
 double * * create_u_lab(long int size, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income, double * lab_max);
 void destroy_matrix(double * * matrix, int size);
 double * * create_lab_matrix(double * * ulab_matrix, int size, double l_max, double delta);
 void query1(double * * lab_matrix, int size, int source);
+void query3(double * * lab_matrix, int size, int source);
+void get_input(int * size, double * delta_1, double * delta_2, int * source, double * alpha, FILE * fp);
 int main(int argc, char * * argv)
 {
-	long int size = 0;
+	double * * ulab_matrix = NULL;
+	double * * dense_graph = NULL;
+	double * * sparse_graph = NULL;
+	double l_max = 0.0;
+	double delta_1 = 0.0;
+	double delta_2 = 0.0;
+	int size = 0;
+	double alpha = 0.0;
+	int source = 0;
 	FILE * fp = fopen(argv[1], "r");
 	if(fp == NULL)
 	{
 		printf("Error in opening file");
-		return EXIT_FAILURE;
 	}
-	char * str = malloc(sizeof(char) * 2048);
-	while(fgets(str, 2048, fp))
-	{
-		size++;
-	}
+	get_input(&size, &delta_1, &delta_2, &source, &alpha, fp);
 	int * age = malloc(sizeof(int) * size);
 	int * gender = malloc(sizeof(int) * size);
 	int * m_status = malloc(sizeof(int) * size);
@@ -32,20 +37,21 @@ int main(int argc, char * * argv)
 	int * lang = malloc(sizeof(int) * size);
 	int * occupation = malloc(sizeof(int) * size);
 	int * income = malloc(sizeof(int) * size);
-	double * * ulab_matrix = NULL;
-	double * * lab_matrix = NULL;
-	double l_max = 0.0;
-	double delta = 0.3;
-	fclose(fp);
-	create_arr(size, argv[1], age, gender, m_status, race, birth, lang, occupation, income);
-	print_arr(size, age, gender, m_status, race, birth, lang, occupation, income);
+	create_arr(size, argv[1], age, gender, m_status, race, birth, lang, occupation, income, fp);
+	//print_arr(size, age, gender, m_status, race, birth, lang, occupation, income);
 	ulab_matrix = create_u_lab(size, age, gender, m_status, race, birth, lang, occupation, income, &l_max);
-	lab_matrix = create_lab_matrix(ulab_matrix, size, l_max, delta);
-	query1(lab_matrix, size, 5);
+	dense_graph = create_lab_matrix(ulab_matrix, size, l_max, delta_1);
+	printf("\n\n");
+	sparse_graph = create_lab_matrix(ulab_matrix, size, l_max, delta_2);
+	query1(dense_graph, size, source);
+	//printf("\n\n");
+	//query1(sparse_graph, size, source);
+	query3(dense_graph, size, source);
+	//query3(sparse_graph, size, source);
 	destroy_arr(age, gender, m_status, race, birth, lang, occupation, income);
 	destroy_matrix(ulab_matrix, size);
-	destroy_matrix(lab_matrix, size);
-	free(str);
+	destroy_matrix(dense_graph, size);
+	destroy_matrix(sparse_graph, size);
 	return EXIT_SUCCESS;
 }
 void destroy_matrix(double * * matrix, int size)
@@ -76,19 +82,13 @@ void print_arr(long int size, int * age, int * gender, int * m_status, int * rac
 		printf("%d, %d, %d, %d, %d, %d, %d, %d\n", age[lcv], gender[lcv], m_status[lcv], race[lcv], birth[lcv], lang[lcv], occupation[lcv], income[lcv]);
 	}
 }
-void create_arr(long int size, char * file, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income)
+void create_arr(int size, char * file, int * age, int * gender, int * m_status, int * race, int * birth, int * lang, int * occupation, int * income, FILE * fp)
 {
 	int user_id = 0;
 	int lcv = 0;
-	FILE * fp = fopen(file, "r");
-	if(fp == NULL)
-	{
-		printf("Error in opening file");
-		return;
-	}
 	for(lcv = 0; lcv < size; lcv++)
 	{
-		fscanf(fp, "%d, %d, %d, %d, %d, %d, %d, %d, %d", &user_id, &age[lcv], &gender[lcv], &m_status[lcv], &race[lcv], &birth[lcv], &lang[lcv], &occupation[lcv], &income[lcv]);
+		fscanf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d", &user_id, &age[lcv], &gender[lcv], &m_status[lcv], &race[lcv], &birth[lcv], &lang[lcv], &occupation[lcv], &income[lcv]);
 	}
 	fclose(fp);
 }
@@ -164,5 +164,37 @@ void query1(double * * lab_matrix, int size, int source)
 	for(lcv = 0; lcv < ctr; lcv++)
 		printf("The lowest LAB nodes are %d\n", query_1[lcv]);
 	printf("The smallest LAB value is: %lf\n", min);
+	free(query_1);
 }
-
+void query3(double * * lab_matrix, int size, int source)
+{
+	int * query_3 = malloc(sizeof(int) * size);
+	int ctr = 0;
+	int lcv = 0;
+	for(lcv = 0; lcv < size; lcv++)
+	{
+		if(lab_matrix[source - 1][lcv] != 0)
+		{
+			query_3[ctr] = lcv + 1;
+			ctr++;
+		}
+	}
+	printf("The number of immdiate neigbours for %d are: %d\n", source, ctr);
+	for(lcv = 0; lcv < ctr; lcv++)
+		printf("The node ID's are %d\n", query_3[lcv]);
+	free(query_3);
+}
+void get_input(int * size, double * delta_1, double * delta_2, int * source, double * alpha, FILE * fp)
+{
+	int size_tmp = 0;
+	double delta1_tmp = 0.0;
+	double delta2_tmp = 0.0;
+	int source_tmp = 0;
+	double alpha_tmp = 0.0;
+	fscanf(fp, "%d,%lf,%lf,%d,%lf", &size_tmp, &delta1_tmp, &delta2_tmp, &source_tmp, &alpha_tmp);
+	* size = size_tmp;
+	* delta_1 = delta1_tmp;
+	* delta_2 = delta2_tmp;
+	* source = source_tmp;
+	* alpha = alpha_tmp;
+}
